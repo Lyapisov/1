3bba1acd70d8f5851788ff2435cd7ef1463df6b6
lyapisov <lyapisov@rambler.ru>
2019-11-12 23:44:21 +0300

    Add corrected form from user

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,11 @@ return array(
     'category/([0-9]+)' => 'catalog/category/$1',
 
     'user/register' => 'user/register',
+    'user/login' => 'user/login',
+    'user/logout' => 'user/logout',
+
+    'cabinet/edit' => 'cabinet/edit',
+    'cabinet' => 'cabinet/index',
 
     '' => 'site/index', // actionIndex в SiteController
     
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -45,4 +45,44 @@ class UserController
         return true;
 
     }
+
+    public function actionLogin(){
+
+        $email ='';
+        $password = '';
+
+        if(isset($_POST['submit'])){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $errors = false;
+
+            if (!User::checkEmail($email)) {
+                $errors[] = 'E-mail Не верный';
+            }
+
+            if (!User::checkPassword($password)) {
+                $errors[] = 'Пароль должен быть не менее 6-ти символов';
+            }
+
+            $userId = User::checkUserData($email, $password);
+
+            if ($userId == false){
+                $errors[] = 'Неверное имя пользователя или пароль';
+            } else {
+                User::auth($userId);
+
+                header("Location: /cabinet/");
+            }
+        }
+
+        require_once(ROOT . '/views/user/login.php');
+        return true;
+    }
+
+    public function actionLogout(){
+
+        unset($_SESSION['user']);
+        header("Location: /");
+    }
 }
